e8f3bb016b49042ae931492956cf90ce1e14afb4
coppy <zheqingli@icloud.com>
2020-01-03 08:56:53 +0800

    fix:#1

--- e2e/tests/moduleStylePropertyCompletions.test.js
+++ e2e/tests/moduleStylePropertyCompletions.test.js
@@ -8,13 +8,14 @@ describe('ModuleStyle Property Completions', () => {
   it('should return details for classNames completion', () => {
     const server = createServer();
     const code = [
+      '// comment',
       `import {useModuleStyle} from 'style-hook'`,
       'const styles = useModuleStyle`.a{}`',
       'styles.',
     ].join('\n')
     openMockFile(server, mockFileName, code);
     const command = 'completions'
-    server.sendCommand(command, { file: mockFileName, offset: 8, line: 3 });
+    server.sendCommand(command, { file: mockFileName, offset: 8, line: 4 });
     return server.close().then(() => {
       const completionsResponse = getFirstResponseOfType(command, server);
       expect(completionsResponse.success).toBeTruthy()
