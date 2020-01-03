f23921014599f08a2468f77068bde4066c127486
coppy <zheqingli@icloud.com>
2020-01-03 14:53:49 +0800

    style: lint test file

--- e2e/tests/moduleStyleDefinition.test.js
+++ e2e/tests/moduleStyleDefinition.test.js
@@ -1,26 +1,27 @@
+/* eslint-disable no-undef */
 const path = require('path')
 const createServer = require('../server-fixture')
-const { openMockFile, getFirstResponseOfType } = require('./helpers');
+const { openMockFile, getFirstResponseOfType } = require('./helpers')
 
-const mockFileName = path.join(__dirname, '..', 'project-fixture', 'app.tsx');
+const mockFileName = path.join(__dirname, '..', 'project-fixture', 'app.tsx')
 
 describe('ModuleStyle property go to Definition', () => {
   it('should go to the style code className', () => {
-    const server = createServer();
+    const server = createServer()
     const code = [
       `import {useModuleStyle} from 'style-hook'`,
       'const styles = useModuleStyle`.a{}`',
       'styles.a',
     ].join('\n')
-    openMockFile(server, mockFileName, code);
+    openMockFile(server, mockFileName, code)
     const command = 'definitionAndBoundSpan'
-    server.sendCommand(command, { file: mockFileName, offset: 9, line: 3 });
+    server.sendCommand(command, { file: mockFileName, offset: 9, line: 3 })
     return server.close().then(() => {
-      const completionsResponse = getFirstResponseOfType(command, server);
+      const completionsResponse = getFirstResponseOfType(command, server)
       expect(completionsResponse.success).toBeTruthy()
       expect(completionsResponse.body.definitions).toHaveLength(1)
       expect(completionsResponse.body.definitions[0].start).toEqual({ line: 2, offset: 32 })
       expect(completionsResponse.body.definitions[0].end).toEqual({ line: 2, offset: 33 })
-    });
-  });
-});
+    })
+  })
+})
--- e2e/tests/moduleStylePropertyCompletions.test.js
+++ e2e/tests/moduleStylePropertyCompletions.test.js
@@ -1,45 +1,46 @@
+/* eslint-disable no-undef */
 const path = require('path')
 const createServer = require('../server-fixture')
-const { openMockFile, getFirstResponseOfType } = require('./helpers');
+const { openMockFile, getFirstResponseOfType } = require('./helpers')
 const typescript = require('typescript')
 
-const mockFileName = path.join(__dirname, '..', 'project-fixture', 'app.tsx');
+const mockFileName = path.join(__dirname, '..', 'project-fixture', 'app.tsx')
 
 describe('ModuleStyle Property Completions', () => {
   it('should return details for classNames completion', () => {
-    const server = createServer();
+    const server = createServer()
     const code = [
       '// comment',
       `import {useModuleStyle} from 'style-hook'`,
       'const styles = useModuleStyle`.a{}`',
       'styles.',
     ].join('\n')
-    openMockFile(server, mockFileName, code);
+    openMockFile(server, mockFileName, code)
     const command = 'completions'
-    server.sendCommand(command, { file: mockFileName, offset: 8, line: 4 });
+    server.sendCommand(command, { file: mockFileName, offset: 8, line: 4 })
     return server.close().then(() => {
-      const completionsResponse = getFirstResponseOfType(command, server);
+      const completionsResponse = getFirstResponseOfType(command, server)
       expect(completionsResponse.success).toBeTruthy()
       expect(completionsResponse.body).toHaveLength(1)
       expect(completionsResponse.body[0].name).toBe('a')
-    });
-  });
-});
+    })
+  })
+})
 
 describe('issue', () => {
   it('#3', () => {
-    const server = createServer();
+    const server = createServer()
     const code = [
-      `import React from 'react'`,
-      `import { createModuleStyle } from 'style-hook'`,
+      'import React from \'react\'',
+      'import { createModuleStyle } from \'style-hook\'',
       'const styles = createModuleStyle `.logo {}`',
       'const JSX = <img className={styles.logo} />',
     ].join('\n')
-    openMockFile(server, mockFileName, code);
+    openMockFile(server, mockFileName, code)
     const command = 'completions'
-    server.sendCommand(command, { file: mockFileName, offset: 41, line: 4 });
+    server.sendCommand(command, { file: mockFileName, offset: 41, line: 4 })
     return server.close().then(() => {
-      const completionsResponse = getFirstResponseOfType(command, server);
+      const completionsResponse = getFirstResponseOfType(command, server)
       expect(completionsResponse.success).toBeTruthy()
       expect(completionsResponse.body).not.toHaveLength(0)
       expect(completionsResponse.body[0].kind).toBe(typescript.ScriptElementKind.jsxAttribute)
